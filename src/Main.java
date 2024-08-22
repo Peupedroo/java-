7e705361eafa9950c41ca769472a03824f7ed531
Pedro Manoel Silva Gouveia <Pedromanoel@academicos.com>
2024-08-22 16:42:19 -0300

    mandando tarefas para o repositorio

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "paracomecar digite 1");
+        int comeco = 0;
+        Scanner sc0 = new Scanner(System.in);
+
+        do {
+            System.out.println("informe sua matricula");
+            Scanner sc1 = new Scanner(System.in);
+
+            System.out.println("informe seu nome ");
+            Scanner sc2 = new Scanner(System.in);
+
+        }while (comeco == 1);
+    }
+}
