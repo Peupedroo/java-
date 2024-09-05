30094fe58ddcca584a67be3675feaffaa93aaf8b
pedroo <pedrosilvagouveia@gmail.com>
2024-09-05 15:56:32 -0300

    enviando pq o pc bugou

---
+++ src3/Funcionario.java
@@ -0,0 +1,11 @@
+public class Funcionario {
+    private int hora;
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getHora() {
+        return hora * 15;
+    }
+}
